999316a73ac2e5c2d11c2d310ac715985ff3604d
ABGursu <64792279+ABGursu@users.noreply.github.com>
2020-05-30 22:34:22 +0300

    BishopOnKingRing
    Midgame bonus for bishops which alligned with the king ring but consider all pawns as blockers.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -128,6 +128,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns         = S(  3,  7);
+  constexpr Score BishopOnKingRing    = S( 16,  0);
   constexpr Score BishopXRayPawns     = S(  4,  5);
   constexpr Score CorneredBishop      = S( 50, 50);
   constexpr Score FlankAttacks        = S(  8,  0);
@@ -290,7 +291,10 @@ namespace {
         }
         else if (Pt == ROOK && (file_bb(s) & kingRing[Them]))
             score += RookOnKingRing;
-
+            
+        else if (Pt == BISHOP && (attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & kingRing[Them]))
+            score += BishopOnKingRing;
+            
         int mob = popcount(b & mobilityArea[Us]);
 
         mobility[Us] += MobilityBonus[Pt - 2][mob];
